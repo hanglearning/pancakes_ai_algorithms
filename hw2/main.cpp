b716df1a5690f17e23cfa7b823bfcf35d33ee976
Hang Chen <hangforjobs@gmail.com>
2018-09-20 21:54:58 -0400

    Initial Commit

---
+++ hw2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hw2
+//
+//  Created by Hang Chen on 9/20/18.
+//  Copyright © 2018 Hang Chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
